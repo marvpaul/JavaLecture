f79eba75548d43f1c74a320e239178461b5509fc
marvpaul <marvin.krueger@yahoo.de>
2018-09-12 21:01:21 +0200

    Version 1

---
+++ src/javaPro/main.java
@@ -0,0 +1,37 @@
+package javaPro;
+
+public class main {
+	public static void main(String[] args) {
+		//Variable definieren
+		//Variablen haben: Datentyp Namen = Wert
+		int zahl = 13; 
+		int zahl2 = 12; 
+		int summe = zahl + zahl2; 
+		
+		
+		// Datentyp string: Zeichenkette
+		// Werte vom Datentyp String können Zeichenketten speichern
+		String datenspeicher = "Hello World 2";
+		
+		//Datentyp boolean: Wahrheitswert
+		//Werte vom Datentyp Boolean können True oder False sein 
+		//== Vergleichsoperator
+		boolean zahlenVergleich = 3 == 3; 
+		
+		//Datentyp char: Zeichen 
+		//Werte vom Datentyp char können EIN Zeichen speichern
+		char buchstabeA = 'a'; 
+		
+		int alpha = 3; 
+		
+		//Datentyp int: Ganze Zahl
+		int ergebnisUnsererBerechnung = 200 + 350 + 170 + 1982; 
+		
+		//Datentyp double: Kommazahl
+		double kommazahl = 3.2; 
+		double meineSumme = kommazahl + 2.8;
+		
+		//Methode zur Ausgabe auf der Konsole
+		System.out.println(zahlenVergleich);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author marvinkruger
+ *
+ */
+module javaProject {
+}
